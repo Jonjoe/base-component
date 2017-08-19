3376b0f6d4b68e23ddd6bcc129ccffa2e6cadaf8
Jonjoe Whitfield <me@jonjoe.io>
2017-08-19 19:39:26 +0100

    better comments

--- component.js
+++ component.js
@@ -5,27 +5,34 @@
 */
 
 // Base Imports ====================================================
+// -----------------------------------------------------------------
 import React from 'react'
 import PropTypes from 'prop-types'
 
 // ... Imports =====================================================
+// -----------------------------------------------------------------
 import './styles.css'
 // import 'something'
 
-// Base stateless component ========================================
-// --- Props:
-//        prop1 - STRING (default 'defaultValue')
-//        prop2 - STRING (default 'defaultValue')
-//        prop3 - STRING (default 'defaultValue')
+// Component Definition ============================================
+// -----------------------------------------------------------------
 const BaseComponent = (props) => (
+  // --- Props:
+  //        prop1 - STRING (default 'defaultValue')
+  //        prop2 - STRING (default 'defaultValue')
+  //        prop3 - STRING (default 'defaultValue')
+
   <p styles={Styles}>This is a base Component</p>
 )
 
 // Component Property Validations ==================================
+// -----------------------------------------------------------------
 BaseComponent.propTypes = {
   prop1: PropTypes.string,
   prop2: PropTypes.string,
   prop3: PropTypes.string
 }
 
+// Export ==========================================================
+// -----------------------------------------------------------------
 export default BaseComponent
