5be5c3eb193d50fae25ec9c477c17b76fbbb67a4
Jonjoe Whitfield <me@jonjoe.io>
2017-08-19 01:25:17 +0100

    comments for component files

--- component.js
+++ component.js
@@ -1,6 +1,17 @@
+/*
+  | BaseComponent =================================================
+  -----------------------------------------------------------------
+  Basic Stateless Component.
+*/
+
+// Base Imports ====================================================
 import React, { Component } from 'react'
 import * as Styles from './styles'
 
+// ... Imports =====================================================
+// import 'something'
+
+// Base stateless component ========================================
 const BaseComponent = () => (
   <p styles={Styles}>This is a base Component</p>
 )
--- styles.js
+++ styles.js
@@ -1,5 +1,12 @@
+/*
+  | Base Component Styles =========================================
+  -----------------------------------------------------------------
+*/
+
+// Import Global Styles ===========================================
 import * as GlobalStyles from '../../stylesheets'
 
+// Component Styles ===========================================
 export const base = {
 
 }
