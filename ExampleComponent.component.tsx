602b41e779d218d4abc6f8be62de54487383fa26
Jonjoe Whitfield <me@jonjoe.io>
2020-04-02 23:40:49 +0100

    base component

---
+++ ExampleComponent.component.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import * as Styled from "./ExampleComponent.styles"
+
+interface ExampleComponentProps {
+  children: React.ReactNode
+}
+
+function ExampleComponent(): JSX.Element {
+  const { children } = props
+
+  return (
+    <Styled.Container>
+      {children}
+    </Styled.Container>
+  )
+}
+
+export default ExampleComponent as React.FC
