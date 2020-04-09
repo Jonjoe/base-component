febcb409189e796c86166c85052a9b3ff5c3b0ae
Jonjoe Whitfield <me@jonjoe.io>
2020-04-09 01:20:48 +0100

    Snapshot tests boilerplate

--- ExampleComponent.spec.tsx
+++ ExampleComponent.spec.tsx
@@ -1,5 +1,14 @@
-describe("ExampleComponent", () => {
-  it("should return true", () => {
-    expect(true).toBe(true)
-  })
-})
+import React from "react";
+import renderer from "react-test-renderer";
+
+import ExampleComponent from "./ExampleComponent.component";
+
+test("ExampleComponent renders correctly", () => {
+  const component = renderer.create(
+    <ExampleComponent />
+  );
+
+  const tree = component.toJSON();
+  
+  expect(tree).toMatchSnapshot();
+});
